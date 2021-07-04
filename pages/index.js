1bc899ecdf959c921ed44ae9f3a8c924884e3e31
taizo-pro <t2s.single@gmail.com>
2021-07-04 11:00:10 +0900

    feat: 雛形作成

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import Results from '../components/Results';
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Nav/>
 
       {/* Results */}
+      <Results/>
     </div>
   )
 }
