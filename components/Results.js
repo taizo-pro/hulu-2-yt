ae5898ce7c624bdbf11ec444846f2da37470df9b
taizo-pro <t2s.single@gmail.com>
2021-07-08 10:58:43 +0900

    feat: flipmove 導入

--- components/Results.js
+++ components/Results.js
@@ -1,11 +1,14 @@
+import FlipMove from "react-flip-move"
+
 function Results({ results }) {
   return (
-    <div>
+    // <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
       console.log("🚀 ~ file: Results.js ~ line 7 ~ Results ~ result", result)
-
       ))}
-    </div>
+    {/* </div> */}
+    </FlipMove>
   );
 }
 
