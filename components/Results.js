f419e9300783d4500b63737f872d75fccb8a4468
taizo-pro <t2s.single@gmail.com>
2021-07-10 08:45:14 +0900

    feat: import component

--- components/Results.js
+++ components/Results.js
@@ -1,3 +1,4 @@
+import Thumbnail from "./Thumbnail"
 import FlipMove from "react-flip-move"
 
 function Results({ results }) {
@@ -5,7 +6,7 @@ function Results({ results }) {
     // <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
     <FlipMove className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
       {results.map((result) => (
-      console.log("🚀 ~ file: Results.js ~ line 7 ~ Results ~ result", result)
+        <Thumbnail key={result.id} result={result} />
       ))}
     {/* </div> */}
     </FlipMove>
