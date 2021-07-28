5390d0582f6762e80c44b39d9669f199c866c42f
taizo-pro <t2s.single@gmail.com>
2021-07-28 10:06:43 +0900

    feat: prefecture

--- utils/requests.js
+++ utils/requests.js
@@ -4,47 +4,47 @@ console.log("🚀 ~ file: requests.js ~ line 2 ~ API_KEY", API_KEY)
 // APIエンドポイント
 export default {
   fetchTrending: {
-    title: "Trending",
+    title: "栃木",
     url: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   },
   fetchTopRated: {
-    title: "Top Rated",
+    title: "群馬",
     url: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
   },
   fetchActionMovies: {
-    title: "Action",
+    title: "山梨",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   },
   fetchComedyMovies: {
-    title: "Comedy",
+    title: "長野",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   },
   fetchHorrorMovies: {
-    title: "Horror",
+    title: "新潟",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   },
   fetchRomanceMovies: {
-    title: "Romance",
+    title: "福島",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
   },
   fetchMystery: {
-    title: "Mystery",
+    title: "山形",
     url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`,
   },
-  fetchSciFi: {
-    title: "Sci-Fi",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
-  },
-  fetchWestern: {
-    title: "Western",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
-  },
-  fetchAnimation: {
-    title: "Animation",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
-  },
-  fetchTV: {
-    title: "TV Movie",
-    url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
-  },
+  // fetchSciFi: {
+  //   title: "Sci-Fi",
+  //   url: `/discover/movie?api_key=${API_KEY}&with_genres=878`,
+  // },
+  // fetchWestern: {
+  //   title: "Western",
+  //   url: `/discover/movie?api_key=${API_KEY}&with_genres=37`,
+  // },
+  // fetchAnimation: {
+  //   title: "Animation",
+  //   url: `/discover/movie?api_key=${API_KEY}&with_genres=16`,
+  // },
+  // fetchTV: {
+  //   title: "TV Movie",
+  //   url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`,
+  // },
 };
