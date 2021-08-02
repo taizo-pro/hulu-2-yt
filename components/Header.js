0635b9b5aaf408ce35e00d53c4629c04f8e7d518
taizo-pro <t2s.single@gmail.com>
2021-08-02 22:17:36 +0900

    feat: HOMEのみ残す

--- components/Header.js
+++ components/Header.js
@@ -14,11 +14,11 @@ function Header() {
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
-        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
-        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
-        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
-        <HeaderItem title="SEARCH" Icon={SearchIcon} />
-        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+        {/* <HeaderItem title="TRENDING" Icon={LightningBoltIcon} /> */}
+        {/* <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} /> */}
+        {/* <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} /> */}
+        {/* <HeaderItem title="SEARCH" Icon={SearchIcon} /> */}
+        {/* <HeaderItem title="ACCOUNT" Icon={UserIcon} /> */}
       </div>
       {/* TODO: header logp 設置 */}
       {/* <Image
