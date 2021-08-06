48d3472a08ca5e064e8a50774f9a2316a3651f4f
taizo-pro <t2s.single@gmail.com>
2021-08-06 09:31:36 +0900

    feat: JSON表示

--- components/Results.js
+++ components/Results.js
@@ -1,9 +1,23 @@
 import Thumbnail from "./Thumbnail"
 
 function Results({ results }) {
+  console.log("🚀 ~ file: Results.js ~ line 4 ~ Results ~ results", results)
   return (
     <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-
+      <ul>
+        {results.map(info => (
+          <li key={info.id}>
+          <p>■{info.area_name}</p>
+          <p>【積雪量】{info.snow_volume}</p>
+          <p>【コース数】{info.course}</p>
+          <p>【住所】{info.address}</p>
+          <p>【リフト代】{info.lift_charges}</p>
+          <p>【営業時間】{info.business_hours}</p>
+          <p>【取得元URL】{info.original_page_url}</p>
+          <br></br>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
